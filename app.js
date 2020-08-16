11d0f629bbe1516d783a37851d280d209778e7dc
Mahek <dmahek1@gmail.com>
2020-08-16 20:09:01 +0530

    Start files

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const express = require ('express')
+const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const ejs = require('ejs')
 
 const app = express()
 
+app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
 
-app.listen(3000,()=> { console.log("Server runing at port 3000");});
+// mongoose.connect("mongodb://localhost:27017/StickyNotesDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+app.get("/",(req,res)=>{
+    res.render("note")
+});
+
+app.listen(3000, () => {
+    console.log("Server runing at port 3000");
+});
